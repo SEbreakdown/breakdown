b0a17945ada206fe428f627cc7a14a18023b8ae0
Mitsch <Michael@home>
2013-05-12 20:09:10 +0200

    Added Cucumber feature: Difficulty Cucumber als JUnit-Test

---
+++ tests/de/se/tinf11b3/breakdown/tests/guitests/SchwierigkeitsgradWaehlen.java
@@ -0,0 +1,41 @@
+package de.se.tinf11b3.breakdown.tests.guitests;
+
+import static org.junit.Assert.assertEquals;
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import cucumber.runtime.PendingException;
+
+public class SchwierigkeitsgradWaehlen {
+
+	@Given("^I started the Application$")
+	public void I_started_the_Application() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^I press “Spiel starten”$")
+	public void I_press_Spiel_starten() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^I should see “Leicht”$")
+	public void I_should_see_Leicht() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^I should see “Mittel”$")
+	public void I_should_see_Mittel() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^I should see “Schwer”$")
+	public void I_should_see_Schwer() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+	
+}
